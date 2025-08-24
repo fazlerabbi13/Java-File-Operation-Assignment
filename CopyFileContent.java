f844c9e63b89235307649c5b7996f7682eda9fb3
fazlerabbi13 <mohammad0132794@gmail.com>
2025-08-24 21:19:49 +0600

    Copy File Content.

---
+++ CopyFileContent.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFileContent {
+      public static void main(String[] args) {
+        try (
+            BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
+            BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+             ) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.newLine();
+            }
+            System.out.println("File copied successfully!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
